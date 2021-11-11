7ce6496a9897a87d2d9a1dd7949eda1654cefbb1
Bartlomiej Drogos <bartekk194@gmail.com>
2021-11-11 17:05:35 +0100

    small changes

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
-from account import views
 from django.contrib.auth import views as auth_views
+from django.urls import path
+
+from account import views
 
 app_name = "account"
 
+
 urlpatterns = [
     path("login/", views.LoginUserView.as_view(), name="login"),
+    path("logout/", views.LogoutUserView.as_view(), name="logout"),
     path("register/", views.RegisterView.as_view(), name="register"),
 ]
