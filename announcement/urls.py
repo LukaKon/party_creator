ad72ef7c237f312d3398eb1f61896dc9dd45408c
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-11-13 00:28:33 +0100

    add contact_form, base modification

--- announcement/urls.py
+++ announcement/urls.py
@@ -6,4 +6,5 @@ app_name = "announcement"
 
 urlpatterns = [
     path('',views.HomeView.as_view(),name='home'),
+    path('contact/',views.ContactView.as_view(),name='contact'),
 ]
--- announcement/views.py
+++ announcement/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
 
+
 class HomeView(generic.TemplateView):
+    """Home page"""
     template_name='announcement/index.html'
+
+class ContactView(generic.TemplateView):
+    """Sending message to service owner"""
+    template_name='announcement/contact.html'
+
+    def is_valid():
+        pass
