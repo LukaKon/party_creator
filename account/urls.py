348a122477448f6ec7565607c97c1bbb946a9205
Bartlomiej Drogos <bartekk194@gmail.com>
2021-11-15 19:43:53 +0100

    password_change repaired, added templates

--- account/urls.py
+++ account/urls.py
@@ -45,6 +45,7 @@ urlpatterns = [
     path(
         "password_reset/", auth_views.PasswordResetView.as_view(
             template_name="password_reset/password_reset_form.html",
+            email_template_name="registration/password_reset_email.html",
             success_url=reverse_lazy('account:password_reset_done')
         ), name="password_reset"
     ),
