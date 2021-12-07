7384b09ff75de70aa45e9805f8eb7a8843893374
lukakon <lukasz.konieczny.lk@protonmail.com>
2021-12-07 22:32:00 +0100

    up: adding pic to base not working

--- static/js/image_preview.js
+++ static/js/image_preview.js
@@ -2,58 +2,61 @@
 
 
 const inputFile = document.querySelector('#images');
-const mainDiv=document.querySelector('#img-num')
+const mainDiv = document.querySelector('#img-num')
 const previewContainer = document.querySelector('#imgPreview');
 
 inputFile.addEventListener('change', function () {
     const files = this.files;
+    if (files) {
+        files_arr = Array.from(files)
 
-    files_arr = Array.from(files)
+        // number of selected pictures
+        const div = document.createElement('div');
+        div.className = 'col';
+        const imgNumber = document.createElement('h3');
+        imgNumber.textContent = `Dodano ${files_arr.length} plików.`;
 
-    // number of selected pictures
-    const div=document.createElement('div');
-    div.className='col';
-        const imgNumber=document.createElement('h3');
-    imgNumber.textContent=`Dodano ${files_arr.length} plików.`;
+        div.appendChild(imgNumber);
+        previewContainer.appendChild(div);
 
-    div.appendChild(imgNumber);
-    previewContainer.appendChild(div);
+        files_arr.forEach((file) => {
+            if (file) {
+                // console.log('file:', file)
 
+                const reader = new FileReader();
+                // console.log('reader:', reader)
 
-    files_arr.forEach((file) => {
-        if (file) {
-            console.log('file:', file)
+                reader.addEventListener('load', function () {
+                    const addImage = document.createElement('div');
+                    addImage.className = 'col';
 
-            const reader = new FileReader();
-            console.log('reader:', reader)
+                    const img = document.createElement('img');
+                    img.className = 'image-preview__image';
+                    img.setAttribute('src', this.result);
+                    // img.setAttribute('src', reader.result);
 
-            reader.addEventListener('load', function () {
-                const addImage = document.createElement('div');
-                addImage.className = 'col';
+                    const imgName = document.createElement('p')
+                    imgName.innerText = file.name
 
-                const img = document.createElement('img');
-                img.className = 'image-preview__image';
-                // img.setAttribute('src', this.result);
-                img.setAttribute('src', reader.result);
+                    addImage.appendChild(img);
+                    addImage.appendChild(imgName);
+                    previewContainer.appendChild(addImage);
+                })
+                reader.readAsDataURL(file);
+            } else {
+                console.log('brak zdjęć...')
+            };
+        });
 
-                const imgName=document.createElement('p')
-                imgName.innerText=file.name
+    } else {
+        console.log('brak zdjęć...')
+        const emptyField = document.createElement('div');
+        emptyField.className = 'col';
 
-                addImage.appendChild(img);
-                addImage.appendChild(imgName);
-                previewContainer.appendChild(addImage);
-            })
-            reader.readAsDataURL(file);
-        } else {
-            console.log('brak zdjęć...')
-            const emptyField = document.createElement('div');
-            emptyField.className = 'col';
+        const h3 = document.createElement('h3');
+        h3.innerText = 'nie wybrano jeszcze zdjęć';
 
-            const h3 = document.createElement('h3');
-            h3.innerText = 'nie wybrano jeszcze zdjęć';
-
-            emptyField.appendChild(h3);
-            previewContainer.appendChild(emptyField);
-        };
-    });
+        emptyField.appendChild(h3);
+        previewContainer.appendChild(emptyField);
+    };
 });
