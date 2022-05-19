1165184c240450490ad8fde13181159bec68792a
Bartek7655 <bartekk194@gmail.com>
2022-05-19 18:50:10 +0200

    profile back + front work fine

--- PARTY/front/src/redux/store.js
+++ PARTY/front/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import { announcementReducer } from "./slices/announcementSlice";
+import {profileReducer} from "./slices/profileSlice";
 
 export const store = configureStore({
     reducer: {
         announcements: announcementReducer,
+        profile: profileReducer,
         // category:categoryReducer,
         // event:eventReducer,
     },
