a87fd442765c3335b15072eeb83f1e1aa9129d8f
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-05-20 19:53:53 +0200

    configure admin page for models

--- PARTY/backend/account/tests/test_models.py
+++ PARTY/backend/account/tests/test_models.py
@@ -7,16 +7,17 @@ from django.test import TestCase
 
 class ModelTests(TestCase):
     """Test models."""
+
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
-        email='test@example.com'
-        password='testpass123'
-        user=get_user_model().objects.create_user(
+        email = "test@example.com"
+        password = "testpass123"
+        user = get_user_model().objects.create_user(
             email=email,
             password=password,
         )
 
-        self.assertEqual(user.email,email)
+        self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
@@ -44,14 +45,14 @@ class ModelTests(TestCase):
 
     def test_create_superuser(self):
         """Test creating a superuser."""
-        email='test@example.com'
-        password='testpass123'
-        user=get_user_model().objects.create_superuser(
+        email = "test@example.com"
+        password = "testpass123"
+        user = get_user_model().objects.create_superuser(
             email=email,
             password=password,
         )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
-        self.assertEqual(user.email,email)
+        self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
