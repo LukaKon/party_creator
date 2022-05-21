ee405d5e2e09e092b3144bc6d3f344689b319a07
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-05-21 19:53:14 +0200

    merging - front

--- PARTY/front/src/redux/store.js
+++ PARTY/front/src/redux/store.js
@@ -1,21 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import { announcementReducer } from "./slices/announcementSlice";
-<<<<<<< HEAD
 import { categoryReducer } from "./slices/categorySlice";
-=======
-import {profileReducer} from "./slices/profileSlice";
->>>>>>> 108ae43dc01d3cabc6df81680b12f926eb2f368a
+import { profileReducer } from "./slices/profileSlice";
 
 export const store = configureStore({
     reducer: {
         announcements: announcementReducer,
-<<<<<<< HEAD
         categories: categoryReducer,
-=======
         profile: profileReducer,
-        // category:categoryReducer,
->>>>>>> 108ae43dc01d3cabc6df81680b12f926eb2f368a
         // event:eventReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
