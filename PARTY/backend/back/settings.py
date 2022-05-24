8de8bc0fbf18111332bfac8057110570e4130c38
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-05-24 10:08:34 +0200

    rm staticfiles_dirs - not needed

--- PARTY/backend/back/settings.py
+++ PARTY/backend/back/settings.py
@@ -155,7 +155,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / "static")  # TODO: to change
+    # os.path.join(BASE_DIR / "static")
     # os.path.join(BASE_DIR, "../front/build/static")
 ]
 
