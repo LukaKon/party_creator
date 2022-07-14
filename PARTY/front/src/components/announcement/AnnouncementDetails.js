724a3c5fe2d84520e13aa5ec417a76251df919fe
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-07-14 21:22:42 +0200

    working on announcement details: display data

--- PARTY/front/src/components/announcement/AnnouncementDetails.js
+++ PARTY/front/src/components/announcement/AnnouncementDetails.js
@@ -1,39 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchAnnouncementDetails } from "../../redux/slices/announcementDetailSlice";
-import { Grid, Typography } from "@mui/material";
+import {
+  Box,
+  Grid,
+  Typography,
+} from "@mui/material";
 
 // https://youtu.be/dCbfOZurCQk
 
 export const AnnouncementDetails = () => {
-    const { slug } = useParams();
-    const { loading, entities, error } = useSelector(
-        (state) => state.announcementDetails
-    );
+  const { slug } = useParams();
+  const { loading, entities, error } = useSelector(
+    (state) => state.announcementDetails
+  );
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-    // const [data, setData] = useState();
+  useEffect(() => {
+    dispatch(fetchAnnouncementDetails(slug));
+  }, []);
 
-    useEffect(() => {
-        // axiosInstance.get(slug).then(res=>)...
-        dispatch(fetchAnnouncementDetails(slug));
-    }, []);
+  // console.log("entities: ", entities);
 
-    // TODO: entities is an object but I have no access to details of it
-    console.log("entities: ", entities.id);
-    // console.log("title: ");
+  let content
+  if (loading) {
+    content = <Typography variant="h3">Fetching in progress...</Typography>
+  } else {
+    if (!entities) {
+      content = (<Typography variant="h3">No details!</Typography>)
+    } else {
+      content = (
+        <Box pa sx={{ flexGrow: 1 }}>
+          <Grid container>
+            <Typography variant='h3'>Title: {entities.title}</Typography>
+            <Typography variant="body1">Description: {entities.description}</Typography> 
+          </Grid>
+        </Box>
+      )
+    }
+  }
 
-    return (
-        <Grid container>
-            <div>TODO: get data from server</div>
-            <div>{slug}</div>
-            <Grid>
-                {/* <Typography>Title: {entities.title}</Typography> */}
-            </Grid>
-            <Grid>
-                <Typography>Description: {}</Typography>
-            </Grid>
-        </Grid>
-    );
+  return (
+    <Grid container>
+      <div>slug: {slug}</div>
+      {content}
+    </Grid>
+  );
 };
