a48a3031eccebb705d3f4beb0fbd8112c920a4a2
Bartek7655 <bartekk194@gmail.com>
2022-07-19 19:51:00 +0200

    deleted announcement made by Bartek

--- PARTY/front/src/components/Profile.js
+++ PARTY/front/src/components/Profile.js
@@ -1,7 +1,6 @@
 import React, {useEffect} from 'react'
 import {Grid, Typography} from "@mui/material";
 import {useSelector} from "react-redux";
-import {Announcement} from "./Announcement";
 import {AnnouncementSkeleton} from "./skeletons/AnnouncementSkeletons";
 import {AnnouncementItem} from "./announcement/AnnouncementItem";
 
@@ -44,19 +43,7 @@ export const Profile = () => {
                                 </Grid>
                             );
             })}
-
-            {/*<Grid item container xs={12}>*/}
-            {/*    {entities.announcements.map(ann => {*/}
-            {/*        return (*/}
-            {/*            <Grid item key={ann.title + '1'} xs={12} md={6}>*/}
-            {/*                <Announcement {...ann}/>*/}
-            {/*            </Grid>*/}
-            {/*        )*/}
-            {/*    })}*/}
-            {/*</Grid>*/}
-
         </Grid>
-        // console.log(Object.keys(entities.announcements).map((key) => [Number(key), entities.announcements[key]]));
     }
 
 
