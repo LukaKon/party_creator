aab12cb99d29e2cdb6b904024c0d013f4248edf7
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-11-03 21:02:01 +0100

    form: mv category component back to addannouncement

--- PARTY/front/src/components/announcement/AddAnnouncement.js
+++ PARTY/front/src/components/announcement/AddAnnouncement.js
@@ -3,32 +3,51 @@ import { useSelector } from "react-redux";
 import {
   Button,
   Container,
+  FormControl,
   Grid,
   ImageList,
   ImageListItem,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Select,
   TextField,
   Typography,
 } from "@mui/material";
+import { useTheme } from '@mui/material/styles'
 import { useDispatch } from "react-redux";
 import { fetchCategories } from "../../redux/slices/categorySlice";
 
 import { useInput } from "./hooks/useInput"
 import { createAnnouncement } from "../../redux/slices/announcementSlice";
 
-// import { TitleInput } from './TitleInput'
-import { SelectCategory } from './SelectCategory'
+// import { SelectCategory } from './SelectCategory'
 import { SelectImages } from './SelectImages'
 
 
-export const AddAnnouncement = () => {
 
-  const [dataToSend, setDataToSend] = useState({
-    title: '',
-    description: '',
-    category: [],
-    movies: [],
-    images: [],
-  })
+const ITEM_HEIGHT = 48
+const ITEM_PADDING_TOP = 8
+
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+}
+
+const getStyle = (category, selectedCategory, theme) => {
+  return {
+    fontWeight:
+      selectedCategory.indexOf(category) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium
+  }
+}
+
+export const AddAnnouncement = () => {
 
   const {
     value: enteredTitle,
@@ -48,17 +67,31 @@ export const AddAnnouncement = () => {
     reset: resetDescriptionInput,
   } = useInput(value => value.trim() !== '', '')
 
+
+  const {
+    value: selectedCategory,
+    isValid: selectedCategoryIsValid,
+    hasError: selectedCategoryHasError,
+    valueChangeHandler: selectedCategoryChangedHandler,
+    inputBlurHandler: selectedCategoryBlurHandler,
+    reset: resetSelectedCategory,
+  } = useInput(value => value.length > 0, [])
+
+  const theme = useTheme()
+
+
   const { loading, categories, error } = useSelector(
     (state) => state.categories
   );
 
   const dispatch = useDispatch();
 
-  const [category, setCategory] = useState('')
-  const [categoryValid, setCategoryValid] = useState(false)
+  // const [category, setCategory] = useState('')
+  // const [categoryValid, setCategoryValid] = useState(false)
 
   const [movies, setMovies] = useState([])
   const [selectedImages, setSelectedImages] = useState()
+
 
   // const [resetForm, setResetForm] = useState(false)
 
@@ -69,29 +102,27 @@ export const AddAnnouncement = () => {
 
   let formIsValid = false
 
-  if (enteredTitleIsValid && enteredDescriptionValid && categoryValid) {
+  if (enteredTitleIsValid && enteredDescriptionValid && selectedCategoryIsValid) {
     console.log(
       'title: ', enteredTitleIsValid, enteredTitle,
       'desc: ', enteredDescriptionValid, enteredDescription,
-      'cat: ', categoryValid,category,
+      'cat: ', selectedCategoryIsValid, selectedCategory,
       'images: ', 'not yet',
       'movies: ', movies,
     )
     formIsValid = true
   }
 
-  // console.log('form is valid: ', formIsValid)
-
   const formSubmissionHandler = e => {
     e.preventDefault()
 
-    if (!enteredTitleIsValid && !enteredDescriptionValid && !categoryValid) {
+    if (!enteredTitleIsValid && !enteredDescriptionValid && !selectedCategoryIsValid) {
       return
     }
     const announcement_data = {
       title: enteredTitle,
       description: enteredDescription,
-      category: category,
+      category: selectedCategory,
       images: selectedImages,
       movies: movies,
     }
@@ -100,112 +131,131 @@ export const AddAnnouncement = () => {
 
     resetTitleInput()
     resetDescriptionInput()
-    // setResetForm(true)
-    // setResetForm(false)
+    resetSelectedCategory()
   }
 
-  const categorySelectHandle = (category) => {
-    // console.log('selected cat in children:', category)
-    setCategory(category)
-  }
-
-  const isCategoryValid = (validCategory) => {
-    setCategoryValid(validCategory)
-  }
-
-  // const imageHandleChange = (e) => {
-  //   // console.log('images: ', e.target.files)
-  //   if (e.target.files) {
-  //     const fileArray = Array.from(e.target.files).map(file => URL.createObjectURL(file))
-  //     // console.log('urls: ', fileArray)
-
-  //     // setSelectedImages(prevImages => prevImages.concat(fileArray))
-  //     setSelectedImages(fileArray)
-  //   }
+  // const categorySelectHandle = (category) => {
+  //   setCategory(category)
   // }
 
-  useEffect(() => {
-    dispatch(fetchCategories());
-  }, []);
+  // const isCategoryValid = (validCategory) => {
+  //   setCategoryValid(validCategory)
+// }
+
+// const imageHandleChange = (e) => {
+//   // console.log('images: ', e.target.files)
+//   if (e.target.files) {
+//     const fileArray = Array.from(e.target.files).map(file => URL.createObjectURL(file))
+//     // console.log('urls: ', fileArray)
+
+//     // setSelectedImages(prevImages => prevImages.concat(fileArray))
+//     setSelectedImages(fileArray)
+//   }
+// }
+
+useEffect(() => {
+  dispatch(fetchCategories());
+}, []);
 
 
-  // let images
-  // if (selectedImages) {
-  //   // images = renderPhoto(selectedImages)
-  //   images = <UploadedImageList images={selectedImages} />
-  // } else {
-  //   images = <p>Add some images :)</p>
-  // }
+// let images
+// if (selectedImages) {
+//   // images = renderPhoto(selectedImages)
+//   images = <UploadedImageList images={selectedImages} />
+// } else {
+//   images = <p>Add some images :)</p>
+// }
 
-  let content;
-  if (loading) {
-    content = (<p>Loading...</p>);
-  } else {
-    content = (
-      <Container component="main" maxWidth="xs">
-        <Grid container>
-          <Grid item>
-            <Typography component="h1" variant="h5">
-              Dodaj ogłoszenie:
-            </Typography>
-          </Grid>
+let content;
+if (loading) {
+  content = (<p>Loading...</p>);
+} else {
+  content = (
+    <Container component="main" maxWidth="xs">
+      <Grid container>
+        <Grid item>
+          <Typography component="h1" variant="h5">
+            Dodaj ogłoszenie:
+          </Typography>
+        </Grid>
 
-          <Grid item>
-            <TextField
-              // margin="normal"
-              required
-              id="title"
-              label="Title"
-              name="title"
-              // autoFocus
-              onChange={titleChangedHandler}
-              onBlur={titleBlurHandler}
-              value={enteredTitle}
-              error={titleInputHasError}
-            />
-            {titleInputHasError && (<p style={{ color: "red" }}>Title must not be empty.</p>)}
-          </Grid>
+        <Grid item>
+          <TextField
+            required
+            id="title"
+            label="Title"
+            name="title"
+            // autoFocus
+            onChange={titleChangedHandler}
+            onBlur={titleBlurHandler}
+            value={enteredTitle}
+            error={titleInputHasError}
+          />
+          {titleInputHasError && (<p style={{ color: "red" }}>Title must not be empty.</p>)}
+        </Grid>
 
-          {/*}
-          <Grid item>
-            <TitleInput
-              reset={resetForm}
-            />
-          </Grid>
-        */}
-          <Grid item>
-            <TextField
-              // margin="normal"
-              required
-              id="description"
-              label="Description"
-              name="description"
-              // autoFocus
-              multiline
-              minRows={5}
-              maxRows={10}
-              maxLength={1000}
-              placeholder="Description..."
-              style={{ width: 500 }}
-              onChange={descriptionChangedHandler}
-              onBlur={descriptionBlurHandler}
-              value={enteredDescription}
-              error={descriptionInputHasError}
-            />
-            {descriptionInputHasError && (<p style={{ color: 'red' }}>Description must not be empty.</p>)}
-          </Grid>
+        <Grid item>
+          <TextField
+            required
+            id="description"
+            label="Description"
+            name="description"
+            // autoFocus
+            multiline
+            minRows={5}
+            maxRows={10}
+            maxLength={1000}
+            placeholder="Description..."
+            style={{ width: 500 }}
+            onChange={descriptionChangedHandler}
+            onBlur={descriptionBlurHandler}
+            value={enteredDescription}
+            error={descriptionInputHasError}
+          />
+          {descriptionInputHasError && (<p style={{ color: 'red' }}>Description must not be empty.</p>)}
+        </Grid>
 
-          <Grid item>
-            <SelectCategory
-              categories={categories}
-              selectedCategory={categorySelectHandle}
-              categoryIsValid={isCategoryValid}
-            // reset={resetForm}
-            />
-            {error && (<p style={{ color: 'red' }}>Error: {error}</p>)}
-          </Grid>
+    {/*
+        <Grid item>
+          <SelectCategory
+            categories={categories}
+            selectedCategory={categorySelectHandle}
+            categoryIsValid={isCategoryValid}
+          />
+          {error && (<p style={{ color: 'red' }}>Error: {error}</p>)}
+        </Grid>
+    */}
 
-          {/*
+    <Grid item>
+      <FormControl sx={{ m: 1, width: 300 }}>
+        <InputLabel id='category-label'>Category</InputLabel>
+        <Select
+          labelId="category-label"
+          id='category'
+          required
+          multiple
+          value={selectedCategory}
+          onChange={selectedCategoryChangedHandler}
+          onBlur={selectedCategoryBlurHandler}
+          error={selectedCategoryHasError}
+          input={<OutlinedInput label='Cat' />}
+          MenuProps={MenuProps}
+        >
+          {categories.map(category => (
+            <MenuItem
+              key={category.uuid}
+              value={category.uuid}
+              style={getStyle(category.get_name, selectedCategory, theme)}
+            >
+              {category.get_name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      {selectedCategoryHasError && (<p style={{ color: "red" }}>Category must be selected.</p>)}
+    </Grid>
+
+        {/*
           <Grid item>
             <input type="file" multiple id='file' onChange={imageHandleChange} />
             <div>
@@ -217,45 +267,44 @@ export const AddAnnouncement = () => {
           </Grid>
     */}
 
-          <Grid item>
-            <span style={{ color: 'red' }}>TEST</span>
-            <SelectImages />
-          </Grid>
-
-
-          <Grid item>
-            <TextField
-              margin="normal"
-              // required
-              id="movies"
-              label="Movies"
-              name="movies"
-              // autoFocus
-              onChange={movieChangeHandler}
-            // onBlur={titleBlurHandler}
-            // value={enteredTitle}
-            // error={titleInputHasError}
-            />
-            {titleInputHasError && (<p style={{ color: "red" }}>Title must not be empty.</p>)}
-          </Grid>
-
-          <Grid item>
-            <Button
-              type="submit"
-              disabled={!formIsValid}
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={formSubmissionHandler}
-            >
-              Save
-            </Button>
-          </Grid>
-
+        <Grid item>
+          <span style={{ color: 'red' }}>TEST</span>
+          <SelectImages />
         </Grid>
-      </Container>
-    );
-  }
-  return <Grid>{content}</Grid>;
+
+
+        <Grid item>
+          <TextField
+            margin="normal"
+            // required
+            id="movies"
+            label="Movies"
+            name="movies"
+            // autoFocus
+            onChange={movieChangeHandler}
+          // onBlur={titleBlurHandler}
+          // value={enteredTitle}
+          // error={titleInputHasError}
+          />
+        </Grid>
+
+        <Grid item>
+          <Button
+            type="submit"
+            disabled={!formIsValid}
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={formSubmissionHandler}
+          >
+            Save
+          </Button>
+        </Grid>
+
+      </Grid>
+    </Container>
+  );
+}
+return <Grid>{content}</Grid>;
 };
 
 
