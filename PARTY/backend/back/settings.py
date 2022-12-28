3ec4c5bc37c15192fc94d331df5219fee678c05e
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-12-28 21:56:19 +0100

    up settings... still cant access to vm

--- PARTY/backend/back/settings.py
+++ PARTY/backend/back/settings.py
@@ -11,8 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
 
-# ALLOWED_HOST = config("ALLOWED_HOST", "localhost")
-ALLOWED_HOST = ['*']
+ALLOWED_HOST = config("ALLOWED_HOST", "localhost")
+# ALLOWED_HOST = ['*']
 
 # Application definition
 
