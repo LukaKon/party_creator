dc3103fda85a8c8c6bd204b30b872deb0c050a8c
lukakon <lukasz.konieczny.lk@protonmail.com>
2022-12-30 22:35:25 +0100

    added comment in settings

--- PARTY/backend/back/settings.py
+++ PARTY/backend/back/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
 
-ALLOWED_HOST = (config("ALLOWED_HOST", "localhost"),)
+ALLOWED_HOST = (config("ALLOWED_HOST", "localhost"),)   # probably we don't need that line because it is standard...
 ALLOWED_HOSTS = (config("ALLOWED_HOSTS"),)
 
 # Application definition
