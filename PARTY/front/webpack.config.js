8deaff3328474538efa40cafb4db970c8249309c
lukakon <lukasz.konieczny.lk@protonmail.com>
2023-02-17 18:27:37 +0100

    support for typescript... dont know if it is working

--- PARTY/front/webpack.config.js
+++ PARTY/front/webpack.config.js
@@ -4,59 +4,66 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 
 const config = {
-    mode: "development",
-    entry: {
-        bundle: path.resolve(__dirname, "src/index.js"),
-    },
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
 
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "[name][contenthash].js",
-        clean: true,
-        assetModuleFilename: "[name][ext]",
-        publicPath: '/',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: {
-                    loader: "babel-loader",
-                },
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-        ],
-    },
-    devtool: "source-map",
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, "dist"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: "babel-loader",
         },
-        port: 3000,
-        open: true,
-        hot: true,
-        compress: true,
-        historyApiFallback: true,
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "PartyWizard",
-            // templateContent: ({ htmlWebpackPlugin }) =>
-            // '<!DOCTYPE html><html><head><meta charset="utf-8"><title>' +
-            // htmlWebpackPlugin.options.title +
-            // '</title></head><body><div id="root"></div></body></html>',
-            template: "./src/template.html",
-            filename: "index.html",
-        }),
-        new Dotenv({
-            path: ".env",
-        }),
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx$/,
+        use: {
+          loader: "ts-loader",
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "PartyWizard",
+      // templateContent: ({ htmlWebpackPlugin }) =>
+      // '<!DOCTYPE html><html><head><meta charset="utf-8"><title>' +
+      // htmlWebpackPlugin.options.title +
+      // '</title></head><body><div id="root"></div></body></html>',
+      template: "./src/template.html",
+      filename: "index.html",
+    }),
+    new Dotenv({
+      path: ".env",
+    }),
+  ],
 };
 
 module.exports = config;
